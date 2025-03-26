93becf0a8994f4e2090b57787050185d8deebb18
Jesus-Arman0 <vh0105032024@unab.edu.sv>
2025-03-26 14:35:08 -0600

    Cotactos y clases [Jesús]

--- EL/Contactos.cs
+++ EL/Contactos.cs
@@ -8,5 +8,10 @@ namespace EL
 {
     class Contactos
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+
     }
 }

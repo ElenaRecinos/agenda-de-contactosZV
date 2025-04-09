ff5531fc90975c010ba098be94b9eabe388e6d5f
Jesus-Arman0 <vh0105032024@unab.edu.sv>
2025-04-09 13:38:04 -0600

    Solución EL commit

---
+++ EL/EL/Contacto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EL
+{
+    //Para contactos
+    [Table("Contactos")]
+    public class Contacto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+
+        public List<Telefono> Telefonos { get; set; }
+        public Grupo Grupo { get; set; }
+
+        public Contacto()
+        {
+            Telefonos = new List<Telefono>();
+        }
+    }
+}
---
+++ EL/GUI/FormContacto.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BLL;
+using EL;
+
+namespace GUI
+{
+    public partial class FormContacto: Form
+    {
+        public FormContacto()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               
+                EL.Contacto contacto = new EL.Contacto
+                {
+                    Nombre = txtNombre.Text,
+                    Apellido = txtApellido.Text,
+                    Email = txtEmail.Text
+                    
+                };
+
+               
+                BLL.ContactoBLL servicio = new BLL.ContactoBLL();
+                servicio.Insertar(contacto);
+
+                MessageBox.Show("Contacto guardado con éxito.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al guardar el contacto: " + ex.Message);
+            }
+        }
+    }
+}
